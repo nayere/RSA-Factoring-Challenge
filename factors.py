36143c0450fd7ca998cd3cb9c18e9b3e5c433beb
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-12-13 22:42:26 +0300

    Create factors.py

---
+++ factors.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def factorize(value):
+""""Takes an integer value as input and finds its factors using a basic algorithm.
+Starts i at 2 and iterates through values of i until it finds the first factor of value.
+Prints the factorization in a specific format."""
+"""This script essentially reads a file containing integers and factorizes each integer using a basic factorization algorithm."""
+i = 2
+if value < 2:
+  return
+  print()
+  print(value, "<- value-bef")
+  while value % I:
+    i += 1
+    print("{:.0f}={:.0f}*{:.0f}".format(value, value / i, i))
+    print(value, "<- value-aft")
+    print()
+    
+    if len(argv) != 2:
+      exit()
+
+try:
+  with open(argv[1]) as file:
+      line = file.readline()
+      
+  while line != "":
+          value = int(line.split('\n')[0])
+          factorize(value)
+          line = file.readline()
+except:
+  pass
